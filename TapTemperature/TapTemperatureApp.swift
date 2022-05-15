8efc87c877a18f06ea87b2bc2d38d27c6dab6b3a
aaaa <aaaa>
2022-05-15 10:07:34 +0900

    Initial Commit

---
+++ TapTemperature/TapTemperatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapTemperatureApp.swift
+//  TapTemperature
+//
+//  Created by nnname on 2022/05/15.
+//
+
+import SwiftUI
+
+@main
+struct TapTemperatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
