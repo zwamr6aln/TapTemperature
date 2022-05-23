e552b531bd01b5be721998cf0524fd97f2bbaa2a
aaaa <aaaa>
2022-05-23 13:11:19 +0900

    a

--- TapTemperature/🛠Menu.swift
+++ TapTemperature/🛠Menu.swift
@@ -5,15 +5,13 @@ import HealthKit
 
 struct 🛠MenuButton: View {
     
-    @State private var 🚩Menu: Bool = false
+    @State private var 🚩Menu: Bool = true
     
     @AppStorage("Unit") var 🛠Unit: 🄴numUnit = .℃
     
     @AppStorage("小数点2桁") var 🚩小数点2桁: Bool = false
     
     @AppStorage("自動完了") var 🚩自動完了: Bool = false
-    
-    @AppStorage("ヘルスケアアプリ自動起動") var 🚩ヘルスケアアプリ自動起動: Bool = false
     
     var body: some View {
         Button {
@@ -34,36 +32,27 @@ struct 🛠MenuButton: View {
                         } label: {
                             Label("℃  /  ℉", systemImage: "ruler")
                         }
+                        
+                        Toggle(isOn: .constant(false)) {
+                            Label("基礎体温", systemImage: "bed.double")
+                        }
+                    } header: {
+                        Text("Option")
                     }
                     
                     
                     Section {
+                        Toggle(isOn: $🚩自動完了) {
+                            Label("自動完了", systemImage: "circle.slash.fill")
+                                .symbolRenderingMode(.hierarchical)
+                        }
+                        
                         Toggle(isOn: $🚩小数点2桁) {
-                            HStack {
-                                Image(systemName: "character.cursor.ibeam")
-                                    .foregroundColor(.accentColor)
-                                
-                                Text("36.1\(🛠Unit.rawValue)")
-                                    .padding(.leading, 8)
-                                
-                                Image(systemName: "arrow.right")
-                                    .imageScale(.small)
-                                
-                                Text("36.12︭\(🛠Unit.rawValue)")
-                                    .fontWeight(.semibold)
-                            }
+                            Label("36.1\(🛠Unit.rawValue) → 36.12︭\(🛠Unit.rawValue)",
+                                  systemImage: "character.cursor.ibeam")
                         }
                     } footer: {
                         Text("小数点2桁まで入力する")
-                    }
-                    
-                    
-                    Section{
-                        Toggle("最後まで数字を入力したら自動で完了する", isOn: $🚩自動完了)
-                        
-                        Toggle("ヘルスケアアプリ自動起動", isOn: $🚩ヘルスケアアプリ自動起動)
-                        
-                        Toggle("基礎体温として記録する", isOn: .constant(false))
                     }
                     
                     
